ae1e472582c0f25ec6e7608e10e24e9cd0ef67d9
Witold Olszyk <witoldolszyk@gmail.com>
2017-12-03 22:06:36 +0100

    add solution

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,20 @@ var a = 1,
 console.log('Result with number a: ' + a + ' and number b: ' + b + ' is equal to: '+ value);
 console.log(wynik);
 
-(wynik) ? code_if_condition_is_true : code_if_condition_is_false
 
 
+// inne rozwiązanie
 var a = 1,
     b = 0,
     value = (a*a)-(2*a*b)+(b*b),
     wynik = value > 0,
+    wynik2 = value == 0;
 
 console.log('Result with number a: ' + a + ' and number b: ' + b + ' is equal to: '+ value);
 if (wynik) {
   console.log('wynik dodatni')
+} else if (wynik2) {
+  console.log('wynik równy zero')
 } else {
-  console.log('wynis wynik ujemny')
+  console.log('wynik ujemny')
 }
